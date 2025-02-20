5d236b65ddf0253f2f2284f0fee426ed94426a1a
lengocphan2001 <lengocphan503@gmail.com>
2025-02-20 11:47:41 +0700

    fix: delete unescessary

--- build.sh
+++
@@ -1,5 +0,0 @@
-rm -rf  ./public/index.html
-rm -rf  ./public/assets
-rm -rf  ./public/langs
-npm run build
-cp -r ./dist/* ./public
--- deploy.sh
+++
@@ -1,7 +0,0 @@
-rm -rf vendor
-rm app.zip
-composer install --optimize-autoloader --no-dev
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-zip -r app . -x "/node_modules/*" "composer.lock"
